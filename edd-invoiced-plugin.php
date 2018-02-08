e1e797319973564e1804b3c2506fda226b84b709
debba <andrea.debernardi@dueclic.com>
2018-02-08 11:52:46 +0100

    First commit

---
+++ edd-invoiced-plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Edd Invoiced Plugin
+Plugin URI: https://www.dueclic.com
+Description: Invoice generator for EDD using invoice-generator.com
+Version: 1.0
+Author: dueclic
+Author URI: https://www.dueclic.com
+License: A "Slug" license name e.g. GPL2
+*/
+
